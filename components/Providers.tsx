688daac4eadcca002fcf540b1047e8088c00de54
Prince-The-Billionaire <danielprincewill14@gmail.com>
2024-07-24 13:47:00 +0100

    second

---
+++ components/Providers.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
+import { ReactNode } from 'react'
+
+const client = new QueryClient()
+
+
+const Provider = ({children}:{children:ReactNode}) => {
+    return <QueryClientProvider client={client}>
+        {children}
+    </QueryClientProvider>
+}
+
+export default Providers
